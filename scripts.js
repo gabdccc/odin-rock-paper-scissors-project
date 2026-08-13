a2bbe29cb57a90e77f041c1e0e30a074677ffc85
gab <gabbdc128@gmail.com>
2026-08-14 01:06:08 +0800

    Add the functions of the first part of the game

--- scripts.js
+++ scripts.js
@@ -1,16 +1,36 @@
 // create a function named as getComputerChoice
 // - use Math.random 
-// - return a random number that is greater than or equal to 0 and less than 1
+// - return a random number that is equal to ("rock", "paper", "scissors")
 // >> console.log to make sure the function is working correctly <<
 
+function getComputerChoice() {
+    const random = Math.floor(Math.random() * 3);
+
+    if (random === 2) {
+        return "rock";
+    } if (random === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
 // create a function named as getHumanChoice
 // - use prompt to ask the user to enter either rock, paper, or scissors
 // - return the user's input
 // >> console.log to make sure the function is working correctly <<
 // -- make the parameter case insensitive by using the toLowerCase() method
 
+function getHumanChoice() {
+    let choice = prompt("Rock? Paper? Scissors?").toLowerCase();
+
+    return choice
+}
+
 // create another two variables named as humanScore and computerScore
 // - initialize both variables to 0
+
+let humanScore = 0;
+let computerScore = 0;
 
 // create a function named as playRound
 // - define the function to take two parameters: humanChoice and computerChoice like (playRound(humanChoice, computerChoice))
